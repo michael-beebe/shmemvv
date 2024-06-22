f5c5ff78a71a0a5125d0eef8b434e88bfce6be6d
michael-beebe <michaelbeebe@protonmail.com>
2024-06-21 21:17:53 -0600

    started setting up the 'setup' tests

---
+++ src/tests/setup/setup_tests.cpp
@@ -0,0 +1,42 @@
+#include "setup_tests.hpp"
+
+bool test_shmem_init() {
+  return true;
+}
+
+bool test_shmem_finalize() {
+  return true;
+}
+
+bool test_shmem_my_pe() {
+  return true;
+}
+
+bool test_shmem_n_pes() {
+  return true;
+}
+
+bool test_shmem_pe_accessible() {
+  return true;
+}
+
+bool test_shmem_ptr() {
+  return true;
+}
+
+bool test_shmem_info_get_version() {
+  return true;
+}
+
+bool test_shmem_info_get_name() {
+  return true;
+}
+
+bool test_start_pes() {
+  return true;
+}
+
+bool test_shmem_global_exit() {
+  return true;
+}
+
---
+++ src/tests/setup/setup_tests.hpp
@@ -0,0 +1,18 @@
+#ifndef SETUP_TESTS_HPP
+#define SETUP_TESTS_HPP
+
+#include <shmem.h>
+
+/** Function declarations for setup tests */
+bool test_shmem_init(void);
+bool test_shmem_finalize(void);
+bool test_shmem_my_pe(void);
+bool test_shmem_n_pes(void);
+bool test_shmem_pe_accessible(void);
+bool test_shmem_ptr(void);
+bool test_shmem_info_get_version(void);
+bool test_shmem_info_get_name(void);
+bool test_start_pes(void);
+bool test_shmem_global_exit(void);
+
+#endif /* SETUP_TESTS_HPP */
