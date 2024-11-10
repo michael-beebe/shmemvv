95fcf2f44196388b9fefc2014289c0cf62324170
Yash Kumar <kumaryash6352@gmail.com>
2024-11-10 02:50:45 -0600

    format log.c

--- src/log.c
+++ src/log.c
@@ -4,7 +4,6 @@
 extern "C" {
 #endif
 
-
 /* I'm not a fan of the global either.
  * But,
  * - The log output can change between a file or STDOUT based on initialization.
@@ -12,59 +11,63 @@ extern "C" {
  * - Passing file pointers to every single test is cumbersome.
  * And so a global.
  */
-FILE* LOGGER = NULL;
+FILE *LOGGER = NULL;
 
 /**
   @brief Output a line of information to the global LOGGER.
 
   Prefixed with current time and prefix, in that order. Affixed with \n.
  */
-void log_line(const char* string, const char* prefix) {
-    time_t time_since_epoch = time(NULL);
-    struct tm * timeinfo;
-    struct timeval tv;
+void log_line(const char *string, const char *prefix) {
+  time_t time_since_epoch = time(NULL);
+  struct tm *timeinfo;
+  struct timeval tv;
 
-    time(&time_since_epoch);
-    timeinfo = localtime(&time_since_epoch);
-    gettimeofday(&tv, NULL);
+  time(&time_since_epoch);
+  timeinfo = localtime(&time_since_epoch);
+  gettimeofday(&tv, NULL);
 
-    fprintf(LOGGER, "[%02d:%02d:%02d.%03d] %s%s\n", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, tv.tv_usec / 1000, prefix, string);
+  fprintf(LOGGER, "[%02d:%02d:%02d.%03d] %s%s\n", timeinfo->tm_hour,
+          timeinfo->tm_min, timeinfo->tm_sec, tv.tv_usec / 1000, prefix,
+          string);
 }
 
 /**
    Logs an informational message to the logger.
 
    @param fstring Format string specifying the message to be logged.
-   @param ... Variable arguments to be formatted into the message using `fstring`.
+   @param ... Variable arguments to be formatted into the message using
+   `fstring`.
  */
-void log_info(char* fstring, ...) {
-    va_list va;
-    char buf[MAX_LOG_LEN];
-    va_start(va, fstring);
-    if(vsnprintf(buf, MAX_LOG_LEN, fstring, va) > MAX_LOG_LEN) {
-        log_line(buf, "[INFO] (trunc) ");
-    } else {
-        log_line(buf, "[INFO] ");
-    }
-    va_end(va);
+void log_info(char *fstring, ...) {
+  va_list va;
+  char buf[MAX_LOG_LEN];
+  va_start(va, fstring);
+  if (vsnprintf(buf, MAX_LOG_LEN, fstring, va) > MAX_LOG_LEN) {
+    log_line(buf, "[INFO] (trunc) ");
+  } else {
+    log_line(buf, "[INFO] ");
+  }
+  va_end(va);
 }
 
 /**
    Logs a warning message to the logger.
 
    @param fstring Format string specifying the message to be logged.
-   @param ... Variable arguments to be formatted into the message using `fstring`.
+   @param ... Variable arguments to be formatted into the message using
+   `fstring`.
  */
-void log_warn(char* fstring, ...) {
-    va_list va;
-    char buf[MAX_LOG_LEN];
-    va_start(va, fstring);
-    if(vsnprintf(buf, MAX_LOG_LEN, fstring, va) > MAX_LOG_LEN) {
-        log_line(buf, "[WARN] (trunc) ");
-    } else {
-        log_line(buf, "[WARN] ");
-    }
-    va_end(va);
+void log_warn(char *fstring, ...) {
+  va_list va;
+  char buf[MAX_LOG_LEN];
+  va_start(va, fstring);
+  if (vsnprintf(buf, MAX_LOG_LEN, fstring, va) > MAX_LOG_LEN) {
+    log_line(buf, "[WARN] (trunc) ");
+  } else {
+    log_line(buf, "[WARN] ");
+  }
+  va_end(va);
 }
 
 /**
@@ -73,46 +76,53 @@ void log_warn(char* fstring, ...) {
    Calling this function does not cause the program to exit.
 
    @param fstring Format string specifying the message to be logged.
-   @param ... Variable arguments to be formatted into the message using `fstring`.
+   @param ... Variable arguments to be formatted into the message using
+   `fstring`.
  */
-void log_fail(char* fstring, ...) {
-    va_list va;
-    char buf[MAX_LOG_LEN];
-    va_start(va, fstring);
-    if(vsnprintf(buf, MAX_LOG_LEN, fstring, va) > MAX_LOG_LEN) {
-        log_line(buf, "[FAIL] (trunc) ");
-    } else {
-        log_line(buf, "[FAIL] ");
-    }
-    va_end(va);
+void log_fail(char *fstring, ...) {
+  va_list va;
+  char buf[MAX_LOG_LEN];
+  va_start(va, fstring);
+  if (vsnprintf(buf, MAX_LOG_LEN, fstring, va) > MAX_LOG_LEN) {
+    log_line(buf, "[FAIL] (trunc) ");
+  } else {
+    log_line(buf, "[FAIL] ");
+  }
+  va_end(va);
 }
 
-void log_routine(const char* routine) {
-    log_line(&routine[0], "------ BEGIN TEST ROUTINE: ");
+void log_routine(const char *routine) {
+  log_line(&routine[0], "------ BEGIN TEST ROUTINE: ");
 }
 
-void log_init(const char* test_name) {
-    if (LOGGER) return;
-    const char* path_prefix = getenv("SHMEMVV_LOG_DIR");
-    if(!path_prefix) path_prefix = "/tmp/";
-    char path_buf[256];
-    snprintf(path_buf, 256, "%s%s.pe%02d.out", path_prefix, basename(strdup(test_name)), shmem_my_pe());
-    /* strcpy(path_buf, strdup(path_prefix)); */
-    /* strlcat(path_buf, basename(strdup(test_name)), 256); */
-    /* strlcat(path_buf, ".pe_", 256); */
-    /* strlcat(path_buf, ".out", 256); */
-    FILE* maybe_file = fopen(path_buf, "w+");
-    if(maybe_file == NULL) {
-        fprintf(stderr, "Failed to open log file (error %d: %s). Writing to STDOUT.\n", errno, strerror(errno));
-        maybe_file = stdout;
-    }
-    LOGGER = maybe_file ? maybe_file : stderr;
-    fprintf(LOGGER, "---------- BEGIN TEST: %s\n", test_name);
+void log_init(const char *test_name) {
+  if (LOGGER)
+    return;
+  const char *path_prefix = getenv("SHMEMVV_LOG_DIR");
+  if (!path_prefix)
+    path_prefix = "/tmp/";
+  char path_buf[256];
+  snprintf(path_buf, 256, "%s%s.pe%02d.out", path_prefix,
+           basename(strdup(test_name)), shmem_my_pe());
+  /* strcpy(path_buf, strdup(path_prefix)); */
+  /* strlcat(path_buf, basename(strdup(test_name)), 256); */
+  /* strlcat(path_buf, ".pe_", 256); */
+  /* strlcat(path_buf, ".out", 256); */
+  FILE *maybe_file = fopen(path_buf, "w+");
+  if (maybe_file == NULL) {
+    fprintf(stderr,
+            "Failed to open log file (error %d: %s). Writing to STDOUT.\n",
+            errno, strerror(errno));
+    maybe_file = stdout;
+  }
+  LOGGER = maybe_file ? maybe_file : stderr;
+  fprintf(LOGGER, "---------- BEGIN TEST: %s\n", test_name);
 }
 
 void log_close(int failed) {
-    fprintf(LOGGER, "---------- END TEST: %s\n", failed == 0 ? "PASSED" : "FAILED");
-    fclose(LOGGER);
+  fprintf(LOGGER, "---------- END TEST: %s\n",
+          failed == 0 ? "PASSED" : "FAILED");
+  fclose(LOGGER);
 }
 
 #ifdef __cplusplus
