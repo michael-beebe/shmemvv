fcab19519e45dcf641a7301c8935a1ddd3dc1294
michael-beebe <michaelbeebe@protonmail.com>
2025-07-09 13:28:46 -0500

    Removed todo.txt. All tests seem to be satisfactory (for now).

--- scripts/test.sh
+++ scripts/test.sh
@@ -51,7 +51,7 @@ hosts=$(srun hostname | sort | uniq | paste -sd, -)
 num_hosts=$(echo "$hosts" | tr ',' '\n' | wc -l)
 
 # --- Set the number of processes per host and the total number of processes
-ppn=32
+ppn=128
 np=$(( $num_hosts * $ppn ))
 
 # --- Create a hostfile with specified slots per host
@@ -97,7 +97,7 @@ mkdir -p $SHMEMVV_LOG_DIR
   --launcher $oshrun \
   --launcher_args "--hostfile $hostfile -x SHMEMVV_LOG_DIR" \
   --np $np \
-  --test_collectives
+  --test_all
 
 # --- Clean up
 rm $hostfile
