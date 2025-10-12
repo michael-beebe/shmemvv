52325c581fb23aec3ea10f328c89294ea03567db
Graysen Gould <gouldgraysen@gmail.com>
2025-10-11 22:43:48 -0500

    updated atomic_fetch

--- src/unit/c11/atomics/c11_shmem_atomic_fetch.c
+++ src/unit/c11/atomics/c11_shmem_atomic_fetch.c
@@ -16,20 +16,22 @@
 #define TEST_C11_SHMEM_ATOMIC_FETCH(TYPE)                                      \
   ({                                                                           \
     log_routine("shmem_atomic_fetch(" #TYPE ")");                              \
+    int mype = shmem_my_pe();                                                  \
     bool success = true;                                                       \
     static TYPE *dest;                                                         \
     static TYPE fetch = 0;                                                     \
     dest = (TYPE *)shmem_malloc(sizeof(TYPE));                                 \
     log_info("shmem_malloc'd %d bytes at %p", sizeof(TYPE), (void *)dest);     \
-    TYPE value = 42;                                                           \
+    TYPE value = 42 + mype; /*some arbitrary number*/                          \
     *dest = value;                                                             \
     log_info("set %p to %d", (void *)dest, (int)value);                        \
     shmem_barrier_all();                                                       \
-    log_info("executing atomic fetch: dest = %p", (void *)dest);               \
-    int mype = shmem_my_pe();                                                  \
-    fetch = shmem_atomic_fetch(dest, mype);                                    \
-    shmem_barrier_all();                                                       \
-    success = (fetch == value);                                                \
+    int fetch_pe = (mype + 1) % shmem_n_pes();                                 \
+    log_info("PE %d: executing atomic fetch from PE %d: dest = %p",mype,       \
+      fetch_pe, (void *)dest);                                                 \
+    fetch = shmem_atomic_fetch(dest, fetch_pe);                                \
+    /*no barrier, routine should be blocking*/                                 \
+    success = (fetch == 42 + fetch_pe);                                        \
     if (!success)                                                              \
       log_fail("atomic fetch on %s did not produce expected value %d, "        \
                "got instead %d",                                               \
@@ -45,31 +47,31 @@
 #define TEST_C11_CTX_SHMEM_ATOMIC_FETCH(TYPE)                                  \
   ({                                                                           \
     log_routine("shmem_atomic_fetch(ctx, " #TYPE ")");                         \
-    bool success = true;                                                       \
-    static TYPE *dest;                                                         \
-    static TYPE fetch = 0;                                                     \
-    dest = (TYPE *)shmem_malloc(sizeof(TYPE));                                 \
-    log_info("shmem_malloc'd %d bytes at %p", sizeof(TYPE), (void *)dest);     \
-    TYPE value = 59; /*Different value for ctx test*/                          \
-    *dest = value;                                                             \
-    log_info("set %p to %d", (void *)dest, (int)value);                        \
-                                                                               \
     shmem_ctx_t ctx;                                                           \
     int ctx_create_status = shmem_ctx_create(0, &ctx);                         \
     if (ctx_create_status != 0) {                                              \
       log_fail("Failed to create context");                                    \
-      shmem_free(dest);                                                        \
       return false;                                                            \
     }                                                                          \
     log_info("Successfully created context");                                  \
                                                                                \
-    shmem_barrier_all();                                                       \
-    log_info("executing atomic fetch with context: dest = %p", (void *)dest);  \
+    bool success = true;                                                       \
     int mype = shmem_my_pe();                                                  \
-    fetch = shmem_atomic_fetch(ctx, dest, mype);                               \
-    shmem_ctx_quiet(ctx);                                                      \
+    static TYPE *dest;                                                         \
+    static TYPE fetch = 0;                                                     \
+    dest = (TYPE *)shmem_malloc(sizeof(TYPE));                                 \
+    log_info("shmem_malloc'd %d bytes at %p", sizeof(TYPE), (void *)dest);     \
+    TYPE value = 52 + mype; /*some arbitrary number, different for ctx*/       \
+    *dest = value;                                                             \
+    log_info("set %p to %d", (void *)dest, (int)value);                        \
+                                                                               \
     shmem_barrier_all();                                                       \
-    success = (fetch == value);                                                \
+    int fetch_pe = (mype + 1) % shmem_n_pes();                                 \
+    log_info("PE %d: executing atomic fetch with context from PE %d:"          \
+      " dest = %p", mype, fetch_pe, (void *)dest);                             \
+    fetch = shmem_atomic_fetch(ctx, dest, fetch_pe);                           \
+    /*no barrier, routine should be blocking*/                                 \
+    success = (fetch == 52 + fetch_pe);                                        \
     if (!success)                                                              \
       log_fail("atomic fetch with context on %s did not produce expected "     \
                "value %d, got instead %d",                                     \
@@ -90,32 +92,38 @@ int main(int argc, char *argv[]) {
   shmem_init();
   log_init(__FILE__);
 
-  int rc = EXIT_SUCCESS;
+  if (!(shmem_n_pes() >= 2)) {
+    log_warn("Not enough PEs to run test (requires 2 PEs, have %d PEs)",
+             shmem_n_pes());
+    if (shmem_my_pe() == 0) {
+      display_not_enough_pes("RMA");
+    }
+    shmem_finalize();
+    return EXIT_SUCCESS;
+  }
+
+  static int result = true;
+  static int result_ctx = true;
 
   /* Test standard atomic fetch operations */
-  bool result = true;
   #define X(type, shmem_types) result &= TEST_C11_SHMEM_ATOMIC_FETCH(type);
     SHMEM_EXTENDED_AMO_TYPE_TABLE(X)
   #undef X
 
-  if (shmem_my_pe() == 0) {
-    display_test_result("C11 shmem_atomic_fetch", result, false);
-  }
+  shmem_barrier_all();
+
+  reduce_test_result("C11 shmem_atomic_fetch", &result, false);
 
   /* Test context-specific atomic fetch operations */
-  bool result_ctx = true;
   #define X(type, shmem_types) result_ctx &= TEST_C11_CTX_SHMEM_ATOMIC_FETCH(type);
     SHMEM_EXTENDED_AMO_TYPE_TABLE(X)
   #undef X
 
-  if (shmem_my_pe() == 0) {
-    display_test_result("C11 shmem_atomic_fetch with ctx", result_ctx, false);
-  }
+  reduce_test_result("C11 shmem_atomic_fetch with ctx", &result_ctx, false);
 
-  if (!result || !result_ctx) {
-    rc = EXIT_FAILURE;
-  }
+  shmem_barrier_all();
 
+  bool rc = result & result_ctx ? EXIT_SUCCESS : EXIT_FAILURE;
   log_close(rc);
   shmem_finalize();
   return rc;
